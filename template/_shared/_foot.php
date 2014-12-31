55261e80ff99590c200af4e42d3c4283a0f0e237
kroy <kroy.xc@gmail.com>
2014-12-31 13:06:22 -0500

    begin to flesh out header/footer

--- template/_shared/_foot.php
+++ template/_shared/_foot.php
@@ -1,3 +1,6 @@
+<footer class='footer'>
+    <p>An Aromatic production by <a href="http://github.com/kroy">kroy</a></p>
+</footer>
 
 </body>
 </html>
--- template/_shared/_head.php
+++ template/_shared/_head.php
@@ -2,10 +2,32 @@
 <html>
 <head>
     <title>Smells Like Fail | <?php if (isset($title)) {echo $title;} else {echo '';} ?></title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
-    <script src="/assets/js/bootstrap.min.js"></script> <!-- Might want to move this to bottom of page -->
+    <link rel='stylesheet' type='text/css' href='/assets/css/bootstrap.min.css'>
+    <script src='/assets/js/jquery-2.1.3.min.js'></script>
+    <script src='/assets/js/bootstrap.min.js'></script> <!-- Might want to move this to bottom of page -->
 </head>
 <body>
-<?php
+<nav class='navbar navbar-default'>
+    <div class='container-fluid'>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class='navbar-header'>
+            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                <span class='sr-only'>Toggle navigation</span>
+                <span class='icon-bar'></span>
+                <span class='icon-bar'></span>
+                <span class='icon-bar'></span>
+            </button>
+            <a class='navbar-brand' href='/'>Smells Like Fail</a>
+        </div>
 
-?>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+            <ul class='nav navbar-nav'>
+
+            </ul>
+            <ul class='nav navbar-nav navbar-right'>
+
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
