8ce674ed2e1375d0bb07403cef2446dcb833ec64
kroy <kroy.xc@gmail.com>
2015-01-01 11:29:00 -0500

    make homepage nicer. center footer

--- template/_shared/_foot.php
+++ template/_shared/_foot.php
@@ -1,6 +1,9 @@
-<footer class='footer'>
-    <p>An Aromatic production by <a href="http://github.com/kroy">kroy</a></p>
-</footer>
+
+<div class='container'>
+    <footer class='footer'>
+        <p>An Aromatic production by <a href='http://github.com/kroy'>kroy</a></p>
+    </footer>
+</div>
 
 </body>
 </html>
--- template/homepage/show.php
+++ template/homepage/show.php
@@ -1,3 +1,6 @@
-<?php
-
-echo "Hello show ";
+<div class='jumbotron'>
+    <div class='container'>
+        <h1>Welcome to Smells Like Fail</h1>
+        <p>We're a <a href='http://www.heroesofnewerth.com' title='Heroes of Newerth'>Heroes of Newerth</a> clan. This website hosts the tools that we use to try to get better.</p>
+    </div>
+</div>
