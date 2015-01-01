ca7f89982cd95f8d45cbf43fc38f6551f23a9b05
kroy <kroy.xc@gmail.com>
2015-01-01 11:29:52 -0500

    add members nav item

--- template/_shared/_head.php
+++ template/_shared/_head.php
@@ -7,27 +7,29 @@
     <script src='/assets/js/bootstrap.min.js'></script> <!-- Might want to move this to bottom of page -->
 </head>
 <body>
-<nav class='navbar navbar-default'>
-    <div class='container-fluid'>
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class='navbar-header'>
-            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
-                <span class='sr-only'>Toggle navigation</span>
-                <span class='icon-bar'></span>
-                <span class='icon-bar'></span>
-                <span class='icon-bar'></span>
-            </button>
-            <a class='navbar-brand' href='/'>Smells Like Fail</a>
-        </div>
+    <nav class='navbar navbar-default'>
+        <div class='container-fluid'>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class='navbar-header'>
+                <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                    <span class='sr-only'>Toggle navigation</span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                </button>
+                <a class='navbar-brand' href='/'>Smells Like Fail</a>
+            </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
-            <ul class='nav navbar-nav'>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                <ul class='nav navbar-nav'>
+                    <li>
+                        <a href='/members' title='[SMLF] Clan Members'>Members</a>
+                    </li>
+                </ul>
+                <ul class='nav navbar-nav navbar-right'>
 
-            </ul>
-            <ul class='nav navbar-nav navbar-right'>
-
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
