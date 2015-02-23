e1ff98c71133566962960781e85e6315dd69be78
kroy <kroy.xc@gmail.com>
2015-02-22 22:42:05 -0500

    add logic to Model_Member for getting the finder object. Might move into Model_Base to make this more consistent accross models

--- phplib/Model/Member.php
+++ phplib/Model/Member.php
@@ -4,9 +4,24 @@ class Model_Member extends Model_Base {
     const TABLE_NAME = 'members';
 
     protected static $field_map = [
-        'id' => self::TYPE_INT,
-        'nickname' => self::TYPE_STRING,
-        'title' => self::TYPE_INT,
+        'id'        => self::TYPE_INT,
+        'nickname'  => self::TYPE_STRING,
+        'title'     => self::TYPE_STRING,
     ];
+
+    private static $finder;
+
+    /**
+     * Returns a finder object for this model to facilitate translation
+     * between the DB and models
+     * @return Finder
+     */
+    public static function getFinder() {
+        if (!isset(self::$finder)) {
+            self::$finder = new Finder_Member();
+        }
+
+        return self::$finder;
+    }
 
 }
