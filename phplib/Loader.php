2c80fffa82ad020c37fc500eb42d8b3daa4f00b3
kroy <kroy.xc@gmail.com>
2015-02-22 22:38:45 -0500

    remove finder regex. Finders now follow the classpath rules like everything else

--- phplib/Loader.php
+++ phplib/Loader.php
@@ -7,7 +7,6 @@
  *
  */
 spl_autoload_register(function($class) {
-    $class = preg_replace('/^(Finder)(_.+)/', "Model$2", $class);
     $class = str_replace('_', '/', $class) . '.php';
     require_once($class);
 });
